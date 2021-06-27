c6a948f3d0fb5182a8e1b2ee35d4d8b1aa271ff0
glitch872 <86266768+glitch872@users.noreply.github.com>
2021-06-27 16:29:22 -0700

    Add files via upload

---
+++ Timer-(windows).py
@@ -0,0 +1,110 @@
+import time
+import os
+val = input(":Press |ENTER| key: ")
+# type "test" as the input for the above zone to gain access to presets
+def convert(a,b,c):
+    return a*3600 + b*60 + c;
+def timer1(a,b,c,d):
+    os.system("cls")
+    while d in range(1, 46800):
+        timer = ' Timer: {:02d}:{:02d}:{:02d}'.format(a, b, c)
+        print(timer, end="\r")
+        d -= 1
+        if c == 0:
+            if b == 0:
+                a -= 1
+                b += 59
+                c += 59
+            else:
+                b -= 1
+                c += 59
+        else:
+            c -= 1
+        time.sleep(1)
+    time.sleep(d)
+    os.system("cls")
+    x = 0
+    while x in range(0, 10):
+        print("Time is up")
+        time.sleep(1)
+        os.system("cls")
+        time.sleep(1)
+        x += 1
+        os.system("cls")
+os.system("cls")
+y = 0
+while y == 0:
+    print("Countdown timer")
+    time.sleep(0.5)
+    print("Options:")
+    time.sleep(0.5)
+    print("(1) Veiw presets")
+    time.sleep(0.5)
+    print("(2) Custom")
+    time.sleep(0.5)
+    print("(3) Exit")
+    time.sleep(0.5)
+    print("````````")
+    time.sleep(0.5)
+    s = int(input("Selection: "))
+    if s == 1:
+        if val == "test":
+            os.system("cls")
+            print("Presets:")
+            print("(1) 1 Minute")
+            print("(2) 5 Minutes")
+            print("(3) 10 Minutes")
+            print("``````````````")
+            sel = int(input("Selection: "))
+            if sel == 1:
+                a = 0
+                b = 1
+                c = 0
+                d = convert(a,b,c)
+                timer1(a,b,c,d)
+            elif sel == 2:
+                a = 0
+                b = 5
+                c = 0
+                d = convert(a,b,c)
+                timer1(a,b,c,d)
+            elif  sel == 3:
+                a = 0
+                b = 10
+                c = 0
+                d = convert(a,b,c)
+                timer1(a,b,c,d)
+            else:
+                os.system("cls")
+        else:
+            os.system("cls")
+            print("Not yet implemented")
+            time.sleep(1)
+            os.system("cls")
+            print("going back to menu in:")
+            z = 5
+            while z in range(1, 6):
+                print( z, end="\r")
+                z -= 1
+                time.sleep(1)
+            os.system("cls")
+    elif s == 2:
+        os.system("cls")
+        print("The Max time limit is 24:59.59")
+        a = int(input("Hours: "))
+        b = int(input("Minutes: "))
+        c = int(input("Seconds: "))
+        d = convert(a,b,c)
+        timer1(a,b,c,d)
+    elif s == 3:
+        os.system("cls")
+        print("Bye")
+        time.sleep(1)
+        y = 1
+    else:
+        os.system("cls")
+        print("That is not an option")
+        time.sleep(5)
+        os.system("cls")
+os.system("cls")
+exit
